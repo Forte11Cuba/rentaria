188369a856553f64c51d23168eaa03fb5f4716c5
Forte11Cuba <fortereyes@yandex.com>
2026-06-08 21:05:41 -0600

    feat(customers): self-service password recovery flow

--- apps/customers/urls.py
+++ apps/customers/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<slug:slug>/account/login/', views.login, name='customer_login'),
     path('<slug:slug>/account/logout/', views.logout, name='customer_logout'),
     path('<slug:slug>/account/activate/<str:token>/', views.activate, name='customer_activate'),
+    path('<slug:slug>/account/recover/', views.recover, name='customer_recover'),
 ]
